3625f34afaea330b395f9bbe8e31aec0d93e870e
kevidn <kevindzakyh25@gmail.com>
2024-04-24 20:01:54 +0700

    FirstCommit

---
+++ database/migrations/2024_04_24_124804_create_agendas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kegiatan');
+            $table->string('tempat');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendas');
+    }
+};
