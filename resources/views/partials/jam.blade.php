5900e39bf3f97d467789c05f9a3b203eb531e72f
kevidn <kevindzakyh25@gmail.com>
2024-05-29 16:24:51 +0700

    fix layout dashboard

--- resources/views/partials/jam.blade.php
+++ resources/views/partials/jam.blade.php
@@ -4,36 +4,71 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Digital Clock</title>
-  <style>
-    body {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: ;
-      background-color: #f2f2f2;
-    }
+  
+  @if (Request::is("index"))
+    <style>
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: ;
+        background-color: #f2f2f2;
+      }
 
-    .clock {
-      font-family: Arial, sans-serif;
-      width: 170px;
-      height: 49px;
-      background-color: #fff;
-      border-radius: 15px;
-      display: flex;
-      justify-content: center;
-      text-align: center;
-      font-size: 45px;
-      color: #fff;
-      background-color: #01263746;
-      /* box-shadow: 0 0 20px rgba(0,0,0,0.5); */
-    }
+      .clock {
+        font-family: Arial, sans-serif;
+        width: 170px;
+        height: 49px;
+        background-color: #fff;
+        border-radius: 15px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-size: 45px;
+        color: #fff;
+        background-color: #01263746;
+        /* box-shadow: 0 0 20px rgba(0,0,0,0.5); */
+      }
 
 
-    .digit {
-      display: inline-block;
-    }
+      .digit {
+        display: inline-block;
+      }
 
-  </style>
+    </style>
+  @else
+    <style>
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: ;
+        background-color: #f2f2f2;
+      }
+
+      .clock {
+        font-family: Arial, sans-serif;
+        width: 110px;
+        height: 40px;
+        background-color: #fff;
+        border-radius: 15px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-size: 35px;
+        color: #fff;
+        background-color: #01263746;
+        /* box-shadow: 0 0 20px rgba(0,0,0,0.5); */
+      }
+
+
+      .digit {
+        display: inline-block;
+      }
+
+    </style>
+  @endif
+
 </head>
 <body>
   <div class="clock">
