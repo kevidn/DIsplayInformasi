06a5227e7c4436711de71b84e18fd4b349e28258
haikal-266 <haikalrizky266@gmail.com>
2024-06-12 19:22:52 +0700

    initial commit

--- resources/views/display/index.blade.php
+++ resources/views/display/index.blade.php
@@ -235,7 +235,7 @@
                             <img src="{{ asset('/images/fatahillah.png') }}" alt="Logo 2" style="height: 75px; width: 75px;">
 
                         </div>
-<<<<<<< HEAD
+
                          {{-- VIDEO DISPLAY --}}
                          @if (Request::is("index"))
                          <div style="display: flex; justify-content:center; margin: 10px;">
@@ -298,79 +298,10 @@
                          @endif
 
 
-=======
-                        {{-- VIDEO DISPLAY --}}
-                        @if (Request::is("index"))
-                        <div style="display: flex; justify-content:center; margin: 10px;">
-                            @if ($videodisplay && $videodisplay->tampil == 1)
-                                @if ($videodisplay->youtubelinks)
-                                    {{-- Jika video dari YouTube --}}
-                                    <iframe width="800" height="375" src="{{ $videodisplay->youtubelinks }}" allow="autoplay" frameborder="0"></iframe>
-                                @elseif ($videodisplay->videolokal)
-                                    {{-- Jika video dari lokal --}}
-                                    <video id="localVideo" width="800" height="375" controls autoplay>
-                                        <source id="videoSource" src="{{ asset('storage/videolokal/' . $videodisplay->videolokal) }}" type="video/mp4">
-                                        Your browser does not support the video tag.
-                                    </video>
-                                @else
-                                    {{-- Jika tidak ada video dari YouTube atau lokal --}}
-                                    <video width="800" height="375" controls autoplay muted loop>
-                                        <source src="{{ asset('videos/dummy.mp4') }}" type="video/mp4">
-                                        Your browser does not support the video tag.
-                                    </video>
-                                @endif
-                            @else
-                                {{-- Jika tidak ada video yang tersedia --}}
-                                <video width="800" height="375" controls autoplay muted loop>
-                                    <source src="{{ asset('videos/dummy.mp4') }}" type="video/mp4">
-                                    Your browser does not support the video tag.
-                                </video>
-                            @endif
-                        </div>
->>>>>>> 2277de04ea77c2b88acf4277b96dc6bc1cf01934
 
 
-=======
-                            @else
-                                {{-- Jika tidak ada video yang tersedia --}}
-                                <video width="800" height="375" controls autoplay muted loop>
-                                    <source src="{{ asset('videos/dummy.mp4') }}" type="video/mp4">
-                                    Your browser does not support the video tag.
-                                </video>
-                            @endif
-                        </div>
-                        @endif
 
 
-                        {{-- VIDEO DASHBOARD --}}
-                        @if (Request::is("dashboard"))
-                        <div style="display: flex; justify-content:center; margin: 10px;">
-                            @if ($videodisplay && $videodisplay->tampil == 1)
-                                @if ($videodisplay->youtubelinks)
-                                    {{-- Jika video dari YouTube --}}
-                                    <iframe width="800" height="375" src="{{ $videodisplay->youtubelinks }}" allow="autoplay" frameborder="0"></iframe>
-                                @elseif ($videodisplay->videolokal)
-                                    {{-- Jika video dari lokal --}}
-                                    <video id="localVideo" width="800" height="375" controls autoplay muted>
-                                        <source id="videoSource" src="{{ asset('storage/videolokal/' . $videodisplay->videolokal) }}" type="video/mp4">
-                                        Your browser does not support the video tag.
-                                    </video>
-                                @else
-                                    {{-- Jika tidak ada video dari YouTube atau lokal --}}
-                                    <video width="800" height="375" controls autoplay muted loop>
-                                        <source src="{{ asset('videos/dummy.mp4') }}" type="video/mp4">
-                                        Your browser does not support the video tag.
-                                    </video>
-                                @endif
-                            @else
-                                {{-- Jika tidak ada video yang tersedia --}}
-                                <video width="800" height="375" controls autoplay muted loop>
-                                    <source src="{{ asset('videos/dummy.mp4') }}" type="video/mp4">
-                                    Your browser does not support the video tag.
-                                </video>
-                            @endif
-                        </div>
-                        @endif
 
 
 
@@ -397,9 +328,7 @@
                             });
                         </script>
 
-<<<<<<< HEAD
-=======
-                  
+
 
 
 
@@ -432,10 +361,9 @@
                                     }
                                 });
                             </script>
-                        @endif
 
 
->>>>>>> 2277de04ea77c2b88acf4277b96dc6bc1cf01934
+
                         <div style="text-align: center; font-size: 25px; font-family: 'Segoe UI'; font-weight: bold; color: white; margin: 15px;">
                             SELAMAT DATANG
                         </div>
