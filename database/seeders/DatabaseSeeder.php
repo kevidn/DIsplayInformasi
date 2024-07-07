506db4c519e02bc31c9716dcfd897987d70f6a87
Kevin Dzaky <115269741+kevidn@users.noreply.github.com>
2024-07-07 16:13:29 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        $this->call(TemaSeeder::class);
         $this->call(RTSeeder::class);
         $this->call(AgendaSeeder::class);
         $this->call(VideoSeeder::class);
