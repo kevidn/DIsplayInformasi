b2ac013ed90c34c456885395bb6cd42ab805974a
Kevin Dzaky <115269741+kevidn@users.noreply.github.com>
2024-07-07 16:12:14 +0700

    Update seeders

---
+++ database/seeders/TemaSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Agenda;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TemaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Agenda::create([
+            'bg' => '/images/bg-blue.jpg',
+            'color' => '#00324946',
+        ]);
+    }
+}
